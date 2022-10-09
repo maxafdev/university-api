48a766fd85c50bf529d36041143e910d61a501f6
Max <maxaf1993@gmail.com>
2022-10-09 16:33:52 +0300

    create spring project.Closed #1
    dependency for liquibase and jpa are copied and will be implemented in other issue.

---
+++ src/main/java/com/coderoom/university/UniversityApiApplication.java
@@ -0,0 +1,13 @@
+package com.coderoom.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityApiApplication.class, args);
+	}
+
+}
